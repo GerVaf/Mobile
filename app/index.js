6928bd1591331e55efbae3bea9a4be3a8b03c529
GerVaf <thantzinmin667@gmail.com>
2023-04-13 21:18:14 +0630

    reactNativeFrame

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { color } from 'react-native-reanimated'
+
+const index = () => {
+  return (
+    <View>
+        <Text style={{color:"black",padding: 10,fontWeight:"bold"}}>React Native Frame</Text>
+    </View>
+  )
+}
+
+export default index
